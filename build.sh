e2d38e4044aa7e6971fb9a192e6574ae7d1d5f66
meixuesong <meixuesong@gmail.com>
2014-12-21 14:05:07 +0800

    写博客

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 rake generate
+echo "生成RSS"
+cd /Users/mxs/Develop/projects/rss2epub/OctopressRSS/bin/
+java -classpath ./:/Users/mxs/Develop/projects/rss2epub/OctopressRSS/jsoup-1.8.1.jar com.ubone.octopress.rss.GenerateOctopressRss /Users/mxs/Documents/blog/public/blog/archives/index.html /Users/mxs/Documents/blog/public/rss.xml http://blog.ubone.com
+cd /Users/mxs/Documents/blog
 rake deploy
 git add .
 git commit -m '写博客'
